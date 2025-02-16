7801b22f54d1b60e53cd6ba51e61278fb7197f18
Corentin Thomasset <corentin.thomasset74@gmail.com>
2025-02-17 00:27:07 +0100

    feat(email-callbacks): add copy email address buttons (#3)

--- apps/client/src/modules/shared/utils/copy.tsx
+++ apps/client/src/modules/shared/utils/copy.tsx
@@ -1,5 +1,7 @@
 import type { ComponentProps, ParentComponent } from 'solid-js';
 import { Button } from '@/modules/ui/components/button';
+import { createToast } from '@/modules/ui/components/sonner';
+import { Tooltip, TooltipContent, TooltipTrigger } from '@/modules/ui/components/tooltip';
 import { createSignal } from 'solid-js';
 
 export function useCopy() {
@@ -26,5 +28,36 @@ export const CopyButton: ParentComponent<{ text: string; label?: string; copiedL
       <div classList={{ 'i-tabler-copy': !getIsJustCopied(), 'i-tabler-check': getIsJustCopied() }} class="mr-2 text-lg" />
       {(getIsJustCopied() ? props.copiedLabel ?? 'Copied!' : props.children ?? props.label ?? 'Copy')}
     </Button>
+  );
+};
+
+export const CopyIconButton: ParentComponent<{ text: string; toast?: string; tooltip?: string } & ComponentProps<typeof Button>> = (props) => {
+  const { copy, getIsJustCopied } = useCopy();
+
+  const handleClick = () => {
+    copy({ text: props.text });
+
+    if (props.toast) {
+      createToast({
+        message: props.toast,
+        icon: <div class="i-tabler-copy size-5 text-muted-foreground mr-2" />,
+      });
+    }
+  };
+
+  return (
+    <Tooltip>
+      <TooltipTrigger
+        as={Button}
+        onClick={handleClick}
+        variant="ghost"
+        size="icon"
+        aria-label="Copy"
+        {...props}
+      >
+        <div classList={{ 'i-tabler-copy': !getIsJustCopied(), 'i-tabler-check': getIsJustCopied() }} />
+      </TooltipTrigger>
+      <TooltipContent>{props.tooltip}</TooltipContent>
+    </Tooltip>
   );
 };
