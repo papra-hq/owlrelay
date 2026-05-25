0d90fc86743de2a381af96907b27f2e6d5494062
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-05-25 15:00:47 +0200

    chore(lint): migrate from eslint to oxlint/oxfmt

---
+++ oxfmt.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'oxfmt';
+
+export default defineConfig({
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'all',
+  printWidth: 200,
+  sortPackageJson: true,
+});
