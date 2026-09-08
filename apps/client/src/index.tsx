af3d230dcb8866b2bc1d6e70774a3d0afc0fdd80
Corentin Thomasset <corentin.thomasset74@gmail.com>
2026-09-08 21:49:43 +0200

    chore(client): remove posthog analytics

--- apps/client/src/index.tsx
+++ apps/client/src/index.tsx
@@ -10,8 +10,6 @@ import { ConfigProvider } from './modules/config/config.provider';
 import { I18nProvider } from './modules/i18n/i18n.provider';
 import { ConfirmModalProvider } from './modules/shared/confirm';
 import { queryClient } from './modules/shared/query/query-client';
-import { IdentifyUser } from './modules/tracking/components/identify-user.component';
-import { PageViewTracker } from './modules/tracking/components/pageview-tracker.component';
 import { Toaster } from './modules/ui/components/sonner';
 import { routes } from './routes';
 import '@unocss/reset/tailwind.css';
@@ -37,8 +35,6 @@ render(() => {
                   // storageManager={localStorageManager}
                 >
                   <Toaster />
-                  <IdentifyUser />
-                  <PageViewTracker />
 
                   <ConfigProvider>
                     <div class="min-h-screen font-sans text-sm font-normal">{props.children}</div>
--- apps/client/src/modules/auth/auth.services.ts
+++ apps/client/src/modules/auth/auth.services.ts
@@ -1,25 +1,8 @@
 import { createAuthClient } from 'better-auth/solid';
 import { buildTimeConfig } from '../config/config';
-import { trackingServices } from '../tracking/tracking.services';
 
-const {
-  useSession,
-  signIn,
-  signUp,
-  signOut: signOutAuth,
-  forgetPassword,
-  resetPassword,
-  sendVerificationEmail,
-} = createAuthClient({
+const { useSession, signIn, signUp, signOut, forgetPassword, resetPassword, sendVerificationEmail } = createAuthClient({
   baseURL: buildTimeConfig.baseApiUrl,
 });
-
-function signOut() {
-  trackingServices.capture({ event: 'User logged out' });
-  const result = signOutAuth();
-  trackingServices.reset();
-
-  return result;
-}
 
 export { forgetPassword, resetPassword, sendVerificationEmail, signIn, signOut, signUp, useSession };
